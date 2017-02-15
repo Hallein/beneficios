87248f6a105d6738d2b67d336eff0b49f5b3e74f
fernando <fernando>
2017-02-15 10:06:59 -0300

    Pequeño cambio en middleware

--- app/middlewares.php
+++ app/middlewares.php
@@ -1,3 +1,41 @@
 <?php 
+	
+	$login = function($request, $response, $next){
+	//antes
+	if( !isset($_SESSION) || empty($_SESSION) ){
+		//crear nueva sesion
+		$data = $request->getParsedBody();
 
-require 'middlewares/login.php';
+		$rut = filter_var($data['user'], FILTER_SANITIZE_STRING);
+		$password = filter_var($data['pass'], FILTER_SANITIZE_STRING);
+
+		$sql = $this->db->prepare('	SELECT 	RUT_PERSONA, 
+											NOMBRE_PERSONA, 
+											APATERNO_PERSONA, 
+											AMATERNO_PERSONA, 
+											CARGO, 
+											CONTRASENA 
+									FROM 	trabajador 
+									WHERE 	RUT_PERSONA = :rut');
+
+		$sql->bindParam(':rut', $rut);
+		$sql->execute();
+		$usuario = $sql->fetch();	
+
+		$password = hash('sha256', $password);
+
+		if(strcmp($password, $usuario['CONTRASENA']) == 0){
+			$_SESSION['usuario'] = $usuario;
+			$response = $next($request, $response);
+
+		}else{
+			$msg['status'] = 'Error';
+			$msg['message'] = 'Contraseña incorrecta';
+		}
+		return $response;
+	}
+	//pasar a ruta
+	//despues
+}
+
+?>
