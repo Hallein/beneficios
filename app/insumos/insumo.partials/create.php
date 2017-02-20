e747e4e7498d103dd9bc33dd0a02207fe73ca80c
Anthelios <julio.caruncho.a@gmail.com>
2017-02-19 23:28:20 -0300

    cositasss

--- app/insumos/insumo.partials/create.php
+++ app/insumos/insumo.partials/create.php
@@ -8,31 +8,31 @@
 		<div class="col-md-12">
 			<form>
 				<div class="row">
-					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+					<div class="col-xs-12 col-sm-6 col-md-6">
 						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-							<input class="mdl-textfield__input" type="text" id="nombre_insumo" value="<?php echo $datos['insumo']['RUT_PERSONA']; ?>">
+							<input class="mdl-textfield__input" type="text" id="nombre_insumo">
 							<label class="mdl-textfield__label" for="nombre_insumo">Nombre</label>
 						</div>	
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+					<div class="col-xs-12 col-sm-6 col-md-6">
 						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 							<input class="mdl-textfield__input" type="text" id="categoria_insumo">
 							<label class="mdl-textfield__label" for="categoria_insumo">Categoria</label>
 						</div>	
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+					<div class="col-xs-12 col-sm-12 col-md-4">
 						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 							<input class="mdl-textfield__input" type="text" id="subcategoria_insumo">
 							<label class="mdl-textfield__label" for="subcategoria_insumo">Subcategoria</label>
 						</div>	
 					</div>	
-					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<div class="col-xs-12 col-sm-6 col-md-4">
 						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 							<input class="mdl-textfield__input" type="text" id="precio_venta">
 							<label class="mdl-textfield__label" for="precio_venta">Precio de venta</label>
 						</div>	
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+					<div class="col-xs-12 col-sm-6 col-md-4">
 						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 							<input class="mdl-textfield__input" type="text" id="precio_compra">
 							<label class="mdl-textfield__label" for="precio_compra">Precio de compra</label>
