73b4f9dc8f22d050ad07dd94cf30f79afca28140
Anthelios <julio.caruncho.a@gmail.com>
2017-07-28 00:29:24 -0400

    ok

--- app/hito_beneficio/hito_beneficio.partials/_create.php
+++ app/hito_beneficio/hito_beneficio.partials/_create.php
@@ -1,1 +1,45 @@
-_create.php
+<div class="col-xs-12 col-md-6">
+	<div class="card">
+		<div class="card-title text-primary-color dark-primary-color">
+			<div class="row center-xs center-sm start-md">
+				<div class="col-xs-12">
+					Ingresar nuevo hito
+				</div>
+			</div>
+		</div>
+		<div class="card-body">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="secondary-text-color">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vitae et cupiditate, eum accusamus quas suscipit vel ex fugiat voluptate illo dolorum repellendus debitis dolore, laboriosam incidunt, ullam ratione. Officiis.</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+					<div class="form-input">
+						<label for="hito">Hito</label>
+						<select id="hito">
+							<option value="">1</option>
+							<option value="">2</option>
+						</select>
+						<div class="input-underline"></div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+					<div class="form-input">
+						<label for="observation">Observación</label>
+						<textarea id="observation"></textarea>
+						<div class="input-underline"></div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<br>
+			<div class="row center-xs">
+				<div class="col-xs-12">
+					<button id="create-button" class="btn btn-primary">Ingresar</button>
+				</div>
+			</div>
+			<br>
+		</div>
+	</div>
+</div>
